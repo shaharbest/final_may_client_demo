bcab5185cbfeadda92c43b9523803ccce2bfb774
Shahar Best <bestshahar9@gmail.com>
2023-06-21 19:14:17 +0300

    test api host name env var

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+const apiHost = import.meta.env.VITE_API_HOST;
+
+console.log(apiHost);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
